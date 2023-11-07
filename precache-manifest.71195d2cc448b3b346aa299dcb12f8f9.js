3907c665f7673ecd91cd550ebaa6b7ab0845115b
Summer Ann <anneun@umich.edu>
2023-11-06 23:07:48 -0500

    Updates

---
+++ precache-manifest.71195d2cc448b3b346aa299dcb12f8f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6d8262f046b67dbf3ad4f45edd687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f02fb43a1deb1230464",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4b4ce6daa3fa7f424c81",
+    "url": "/static/css/main.fd9ea2ee.chunk.css"
+  },
+  {
+    "revision": "6f02fb43a1deb1230464",
+    "url": "/static/js/2.f61a8b97.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.f61a8b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4ce6daa3fa7f424c81",
+    "url": "/static/js/main.10e28693.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
