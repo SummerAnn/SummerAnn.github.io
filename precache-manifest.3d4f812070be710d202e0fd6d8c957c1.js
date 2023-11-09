e830234b591d15b6faa3474a25ccda631ef232e0
Summer Ann <anneun@umich.edu>
2023-11-08 20:59:48 -0500

    Updates

--- precache-manifest.3d4f812070be710d202e0fd6d8c957c1.js
+++ precache-manifest.3d4f812070be710d202e0fd6d8c957c1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48d6e334dc5b17dcec6c634404f58061",
+    "revision": "e950aa261dfeadc7a18e9f2ee9ae15af",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "8c40d11c26130c70d2c2",
+    "revision": "70a86c6d3d314b58d7b1",
     "url": "/static/css/main.fd9ea2ee.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f61a8b97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c40d11c26130c70d2c2",
-    "url": "/static/js/main.245541d6.chunk.js"
+    "revision": "70a86c6d3d314b58d7b1",
+    "url": "/static/js/main.197212f9.chunk.js"
   },
   {
     "revision": "a2d41c56873c741b51b2",
